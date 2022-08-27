ee02ebaefab3488fb30a4ecaa7ae4706cbe221e5
akothapally1 <85452774+akothapally1@users.noreply.github.com>
2022-08-27 23:09:54 +0530

    added all files

---
+++ BulkyBook/BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using BulkyBook.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+      private readonly  ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext  db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+           IEnumerable<Category> objCategoryList =_db.Categories;
+            return View(objCategoryList);
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+           _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+    }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
